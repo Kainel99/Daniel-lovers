18eaa3905439db65c4fb093edfdc5462d2a8c922
janel.hocde <janel.hocde@orange.fr>
2024-05-28 12:52:35 +0000

    home page added

--- connexion.php
+++ connexion.php
@@ -1,20 +1,16 @@
 <?php
 
-$host = "db.3wa.io";
-$port = "3306";
-$dbname = "kilianjanus_daniellovers";
-$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
-
-$user = "kilianjanus";
-$password = "65c31ce01e7314f2e27acf9451035f58";
-
-$db = new PDO(
-    $connexionString,
-    $user,
-    $password
-);
+    $host = "db.3wa.io";
+    $port = "3306";
+    $dbname = "kilianjanus_daniellovers";
+    $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
     
-
-var_dump($db);
-
+    $user = "kilianjanus";
+    $password = "65c31ce01e7314f2e27acf9451035f58";
+    
+    $db = new PDO(
+        $connexionString,
+        $user,
+        $password
+    );
 ?>
